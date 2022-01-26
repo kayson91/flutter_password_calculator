f6e54fcec518ecce1a1bae2cdba2d57448dab9c8
Farouk MOHAMED <kayson91@gmail.com>
2022-01-26 23:32:35 +0000

    Fix export flutter password calculator controller

--- lib/flutter_password_calculator.dart
+++ lib/flutter_password_calculator.dart
@@ -1,5 +1,5 @@
 library flutter_password_calculator;
 
-export 'src/flutter_password_calculator.dart';
+export 'src/flutter_password_calculator_controller.dart';
 export 'src/flutter_password_calculator.dart';
 export 'src/validation_rule.dart';
